b62744521772d8b5c90bfb1cccb01128596e91b3
Krystian Kichewko <krystiankichewko@gmail.com>
2017-04-13 08:30:04 -0700

    First version of console application

---
+++ HearthMirror.Console/Program.cs
@@ -0,0 +1,64 @@
+﻿using Newtonsoft.Json;
+
+namespace HearthMirror.Console
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            string command = ParseArguments(args);
+            ExecuteCommand(command, true);
+        }
+
+        private static string ParseArguments(string[] args)
+        {
+            if (args.Length > 0)
+            {
+                return args[0];
+            }
+            else
+            {
+                return null;
+            }
+        }
+
+        private static void ExecuteCommand(string command, bool main)
+        {
+            object value = null;
+
+            switch (command)
+            {
+                case "cli":
+                    while (command != "exit")
+                    {
+                        command = System.Console.ReadLine();
+                        ExecuteCommand(command, false);
+                    }
+                    break;
+                default:
+                    var type = typeof(Reflection);
+                    var method = type.GetMethod(command);
+                    value = method?.Invoke(null, new object[0]);
+                    break;
+            }
+
+            if (value != null)
+            {
+                System.Console.WriteLine(JsonConvert.SerializeObject(value));
+                if (main)
+                {
+                    System.Environment.Exit(0);
+                }
+            }
+            else
+            {
+                System.Console.WriteLine("Unable to read data.");
+                if (main)
+                {
+                    System.Environment.Exit(1);
+                }
+            }
+        }
+    }
+}
